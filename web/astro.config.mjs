1a26ead8f08113d2d69ff6a75608798f84d57ff4
nicodes <nicozessoules@gmail.com>
2026-04-09 21:19:33 -0600

    add tailwind

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import qwikdev from '@qwikdev/astro';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [qwikdev()]
+  integrations: [qwikdev()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
